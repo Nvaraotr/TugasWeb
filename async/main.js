e5e86cf2a6ba70190116fc9016f7b3e19ef05c71
KaY <104289204+Nvaraotr@users.noreply.github.com>
2023-09-30 15:57:17 +0700

    add async and portoflio web

---
+++ async/main.js
@@ -0,0 +1,12 @@
+const { buyTollRoadCard, topUpBalance, useTollRoad } = require("./utils");
+
+async function getTollAccess() {
+  try {
+    const card = await buyTollRoadCard(25);
+    const updatedCard = await topUpBalance(card, 10);
+    await useTollRoad(updatedCard);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+getTollAccess();
